0fb6e762caf2c5597e1b920141954af0acfb9b44
Douglas Campos <qmx@qmx.me>
2012-06-06 04:28:09 -0300

    updating to the latest api

--- src/test/java/org/dynjs/spec/shims/FailShim.java
+++ src/test/java/org/dynjs/spec/shims/FailShim.java
@@ -1,33 +1,18 @@
 package org.dynjs.spec.shims;
 
 import org.dynjs.api.Function;
-import org.dynjs.api.Scope;
 import org.dynjs.exception.DynJSException;
 import org.dynjs.runtime.DynThreadContext;
 
 public class FailShim implements Function {
+
     @Override
-    public void setContext(DynThreadContext context) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public Object call(Object self, DynThreadContext context, Object... args) {
+        throw new DynJSException(args.length == 1 ? args[0].toString() : "failed");
     }
 
     @Override
-    public Object call(DynThreadContext context, Object[] arguments) {
-        throw new DynJSException(arguments.length == 1 ? arguments[0].toString() : "failed");
-    }
-
-    @Override
-    public Scope getEnclosingScope() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public Object resolve(String name) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void define(String property, Object value) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public String[] getArguments() {
+        return new String[]{"message"};
     }
 }
