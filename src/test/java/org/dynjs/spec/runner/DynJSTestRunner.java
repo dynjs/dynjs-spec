e26e4cee56f4beddee409ef02574270233c50a8a
Bob McWhirter <bob@mcwhirter.org>
2012-10-11 15:38:37 -0400

    Date specs expect America/Los_Angeles TZ.

--- src/test/java/org/dynjs/spec/runner/DynJSTestRunner.java
+++ src/test/java/org/dynjs/spec/runner/DynJSTestRunner.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -228,6 +229,7 @@ public class DynJSTestRunner extends Runner implements Filterable {
 
     private DynJS createDynJSRuntime() {
         Config config = new Config();
+        config.setTimeZone( TimeZone.getTimeZone( "America/Los_Angeles"));
         config.setGlobalObjectFactory(new GlobalObjectFactory() {
             @Override
             public GlobalObject newGlobalObject(DynJS runtime) {
