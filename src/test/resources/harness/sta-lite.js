205da667fec524e55cc237db827409700cdf0ed8
Bob McWhirter <bob@mcwhirter.org>
2012-11-01 10:02:52 -0400

    Add more to our sta-lite.

--- src/test/resources/harness/sta-lite.js
+++ src/test/resources/harness/sta-lite.js
@@ -1,3 +1,11 @@
+function Test262Error(message) {
+    if (message) this.message = message;
+}
+
+Test262Error.prototype.toString = function () {
+    return "Test262 Error: " + this.message;
+};
+
 function testFailed(message) {
     $$$fail(message);
 }
