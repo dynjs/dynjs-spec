4d58048a33814b414e601655553c475d40411107
Bob McWhirter <bob@mcwhirter.org>
2013-01-22 17:02:23 -0500

    Remove out-dated Intl402 tests.

--- src/test/java/org/dynjs/spec/Intl402Test.java
+++ src/test/java/org/dynjs/spec/Intl402Test.java
@@ -1,7 +1,9 @@
 package org.dynjs.spec;
 
 import org.dynjs.spec.runner.SpecTestHelper;
+import org.junit.Ignore;
 
+@Ignore
 public class Intl402Test extends SpecTestHelper {
 	public Intl402Test() {
 		super("intl402");
